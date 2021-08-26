a1aa751c33956b08b9059ade389cd6c72b38c9bf
Rafael <rafael.gcaldas01@gmail.com>
2021-08-25 21:30:29 -0300

    Repositories

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ export function RepositoryItem(props) {
       <strong>{props.repository.name ?? 'Default'}</strong>
       <p>{props.repository.description}</p>
 
-      <a href={props.repository.link}>Acessar repositório</a>
+      <a href={props.repository.html_url} target="_blank">Acessar repositório</a>
     </li>
   );
 }
